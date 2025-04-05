a2712b0644a64f664a07f60643043fb4885b18c5
shivang <shivangagarwal3165@gmail.com>
2025-04-06 00:03:40 +0530

    cors

--- be/src/index.ts
+++ be/src/index.ts
@@ -15,6 +15,13 @@ const anthropic = new Anthropic({
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
+
+
 
 // Endpoint: /template
 app.post("/template", async (req, res) => {
